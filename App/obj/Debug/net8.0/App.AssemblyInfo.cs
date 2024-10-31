f4a8fc9c98b360ce5d6decad856133eabc8d5d96
RenanDRN <renan.reis.negrao@gmail.com>
2024-10-31 19:10:58 -0300

    Ajustando css do App

--- App/obj/Debug/net8.0/App.AssemblyInfo.cs
+++ App/obj/Debug/net8.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fba22663369ad32f8606d0e24d85a1d95ddb374")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8c86909f65583d1c58a954e67d6751c0324478a")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
